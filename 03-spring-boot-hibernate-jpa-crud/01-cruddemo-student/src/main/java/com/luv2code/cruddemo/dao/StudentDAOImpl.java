5fd55350d6420505183821625d4c05c66fadf5fc
hohodsj <hohodsj@gmail.com>
2024-07-12 15:12:23 -0400

    add DAO db access

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,33 @@
+package com.luv2code.cruddemo.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.cruddemo.entity.Student;
+
+import jakarta.persistence.EntityManager;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO{
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // implement save method
+    @Override
+    @Transactional // the one under framwork
+    public void save(Student student) {
+        entityManager.persist(student);
+    }
+    
+}
+
+
+
