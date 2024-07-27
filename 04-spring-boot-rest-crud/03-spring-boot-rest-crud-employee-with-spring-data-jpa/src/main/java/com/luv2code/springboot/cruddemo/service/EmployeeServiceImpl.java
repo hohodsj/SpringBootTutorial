3f4fb0a52f6fe7e6f2160145ba00a947b93260b4
hohodsj <hohodsj@gmail.com>
2024-07-27 13:40:23 -0400

    data jpa

---
+++ 04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,49 @@
+package com.luv2code.springboot.cruddemo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        Optional<Employee> result = employeeRepository.findById(theId);
+        Employee thEmployee = null;
+        if(result.isPresent()) {
+            thEmployee = result.get();
+        } else {
+            throw new RuntimeException("Did not find employee id " + theId);
+        }
+        return thEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeRepository.save(theEmployee);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        employeeRepository.deleteById(theId);
+    }
+    
+}
