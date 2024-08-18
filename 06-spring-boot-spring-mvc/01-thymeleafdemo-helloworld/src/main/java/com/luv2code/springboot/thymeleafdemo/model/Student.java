1c01100192a7ccfc14932907c82e16e75d69f2be
hohodsj <hohodsj@gmail.com>
2024-08-17 22:45:07 -0400

    dynamic load list

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.thymeleafdemo.model;
 public class Student {
     private String firstName;
     private String lastName;
+    private String country;
 
     public Student(){}
 
@@ -20,6 +21,14 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 
     
