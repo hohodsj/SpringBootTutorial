63e0b142f6125a727e93ae027e7d4893e83887ba
hohodsj <hohodsj@gmail.com>
2024-08-24 22:03:13 -0400

    Pattern example with regex

--- 06-spring-boot-spring-mvc/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.mvc;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
@@ -16,6 +17,17 @@ public class Customer {
     @Min(value=0, message="must be greater than or equal to zero")
     @Max(value=10, message="must be less than or equal to zero")
     private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message="only 5 charrs/digits")
+    private String postalCode;
+
+    public String getPostalCode() {
+        return this.postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 
     public int getFreePasses() {
         return this.freePasses;
